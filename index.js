632072c02c2df543d9de35fee2fa689a2435a734
Jacob Friis Saxberg <jacob@weareheadlight.com>
2019-08-19 19:40:56 +0200

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mergeDates = require('./lib/merge-dates');
 const availability = require('./lib/availability');
 
-function findAvailability(start, end, dateStringsArray) {
-  return availability(start, end, mergeDates(dateStringsArray));
+function findAvailability(start, end, timespan, dateStringsArray) {
+  return availability(start, end, timespan, mergeDates(dateStringsArray));
 }
 
 module.exports = findAvailability;
