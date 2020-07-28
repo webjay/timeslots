27c3c1d73fa9e80f5949941fb126053dd003b91c
Jacob Friis Saxberg <jacob@weareheadlight.com>
2020-07-28 15:23:36 +0200

    can be strings or dates

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mergeDates = require('./lib/merge-dates');
 const availability = require('./lib/availability');
 
-function findAvailability(start, end, timespan, dateStringsArray) {
-  return availability(start, end, timespan, mergeDates(dateStringsArray));
+function findAvailability(start, end, timespan, eventsArray) {
+  return availability(start, end, timespan, mergeDates(eventsArray));
 }
 
 module.exports = findAvailability;
