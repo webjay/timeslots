7fbde2eadb420cd2a80cbfbba946631737031666
Jacob Friis Saxberg <jacob@weareheadlight.com>
2020-07-28 15:27:28 +0200

    speed and simplify

--- test/test.js
+++ test/test.js
@@ -2,31 +2,31 @@ const { deepStrictEqual, strictEqual } = require('assert');
 const mergeDates = require('../lib/merge-dates');
 const availability = require('../lib/availability');
 
-const start = new Date('2019-08-10T08:00:00.000Z');
-const end = new Date('2019-08-10T16:00:00.000Z');
+const start = '2019-08-10T08:00:00.000Z';
+const end = '2019-08-10T16:00:00.000Z';
 const timespan = 30 * 60;
 
 const tests = [
-  {
-    name: 'date obj',
-    input: [
-      {
-        start: new Date('2019-08-10T12:00:00+02:00'),
-        end: new Date('2019-08-10T14:00:00+02:00'),
-      },
-      {
-        start: '2019-08-10T13:00:00+02:00',
-        end: '2019-08-10T15:00:00+02:00',
-      },
-    ],
-    expected: [
-      {
-        start: new Date('2019-08-10T10:00:00.000Z'),
-        end: new Date('2019-08-10T13:00:00.000Z'),
-      },
-    ],
-    availabilityCount: (8 * 2) - ((13 - 10) * 2),
-  },
+  // {
+  //   name: 'date obj',
+  //   input: [
+  //     {
+  //       start: new Date('2019-08-10T12:00:00+02:00'),
+  //       end: new Date('2019-08-10T14:00:00+02:00'),
+  //     },
+  //     {
+  //       start: '2019-08-10T13:00:00+02:00',
+  //       end: '2019-08-10T15:00:00+02:00',
+  //     },
+  //   ],
+  //   expected: [
+  //     {
+  //       start: new Date('2019-08-10T10:00:00.000Z'),
+  //       end: new Date('2019-08-10T13:00:00.000Z'),
+  //     },
+  //   ],
+  //   availabilityCount: (8 * 2) - ((13 - 10) * 2),
+  // },
   {
     name: 'extend before',
     input: [
@@ -41,8 +41,8 @@ const tests = [
     ],
     expected: [
       {
-        start: new Date('2019-08-10T09:00:00.000Z'),
-        end: new Date('2019-08-10T12:00:00.000Z'),
+        start: '2019-08-10T09:00:00.000Z',
+        end: '2019-08-10T12:00:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - ((12 - 9) * 2),
@@ -51,18 +51,18 @@ const tests = [
     name: 'extend after',
     input: [
       {
-        start: '2019-08-10T12:00:00+02:00',
-        end: '2019-08-10T14:00:00+02:00',
+        start: '2019-08-10T12:00:00.000Z',
+        end: '2019-08-10T14:00:00.000Z',
       },
       {
-        start: '2019-08-10T13:00:00+02:00',
-        end: '2019-08-10T15:00:00+02:00',
+        start: '2019-08-10T13:00:00.000Z',
+        end: '2019-08-10T15:00:00.000Z',
       },
     ],
     expected: [
       {
-        start: new Date('2019-08-10T10:00:00.000Z'),
-        end: new Date('2019-08-10T13:00:00.000Z'),
+        start: '2019-08-10T12:00:00.000Z',
+        end: '2019-08-10T15:00:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - ((13 - 10) * 2),
@@ -81,8 +81,8 @@ const tests = [
     ],
     expected: [
       {
-        start: new Date('2019-08-10T09:00:00.000Z'),
-        end: new Date('2019-08-10T12:00:00.000Z'),
+        start: '2019-08-10T09:00:00.000Z',
+        end: '2019-08-10T12:00:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - ((12 - 9) * 2),
@@ -101,12 +101,12 @@ const tests = [
     ],
     expected: [
       {
-        start: new Date('2019-08-10T10:00:00.000Z'),
-        end: new Date('2019-08-10T11:00:00.000Z'),
+        start: '2019-08-10T10:00:00.000Z',
+        end: '2019-08-10T11:00:00.000Z',
       },
       {
-        start: new Date('2019-08-10T12:00:00.000Z'),
-        end: new Date('2019-08-10T13:00:00.000Z'),
+        start: '2019-08-10T12:00:00.000Z',
+        end: '2019-08-10T13:00:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - ((11 - 10) * 2) - ((13 - 12) * 2),
@@ -129,8 +129,8 @@ const tests = [
     ],
     expected: [
       {
-        start: new Date('2019-08-10T12:00:00.000Z'),
-        end: new Date('2019-08-10T16:00:00.000Z'),
+        start: '2019-08-10T12:00:00.000Z',
+        end: '2019-08-10T16:00:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - ((16 - 12) * 2),
@@ -145,8 +145,8 @@ const tests = [
     ],
     expected: [
       {
-        start: new Date('2019-08-10T10:00:00.000Z'),
-        end: new Date('2019-08-10T10:45:00.000Z'),
+        start: '2019-08-10T10:00:00.000Z',
+        end: '2019-08-10T10:45:00.000Z',
       },
     ],
     availabilityCount: (8 * 2) - 2,
@@ -160,7 +160,6 @@ describe('Merge dates', () => tests.forEach((test) => {
 describe('Availability', () => tests.forEach((test) => {
   it(test.name, () => {
     const availArray = availability(start, end, timespan, mergeDates(test.input));
-    // console.log(availArray);
     return strictEqual(availArray.length, test.availabilityCount);
   });
 }));
